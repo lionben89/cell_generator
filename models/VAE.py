ef8a7d4602f84f345f6b6755ac1b65c205921c7d
lionben89 <lionben89@gmail.com>
2022-02-13 12:58:25 +0200

    vae first version

---
+++ models/VAE.py
@@ -0,0 +1,143 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import numpy as np
+
+class Sampling(keras.layers.Layer):
+    """Uses (mu, sigma) to sample z."""
+
+    def call(self, inputs):
+        mu, sigma = inputs
+        batch = tf.shape(mu)[0]
+        dim = tf.shape(sigma)[1]
+        epsilon = keras.backend.random_normal(shape=(batch, dim))
+        return mu + tf.exp(0.5 * sigma) * epsilon
+
+def get_encoder(input_size,latent_dim,name="encoder"):
+        layer_dim = np.array(input_size[:-1],dtype=np.int32)
+        filters = 8
+        
+        input = keras.Input(shape=input_size)
+        
+        ## first conv same resulotion
+        x = keras.layers.BatchNormalization(name="{}_bn_{}".format(name,layer_dim[0]))(input)
+        x = keras.layers.Conv3D(filters=filters,kernel_size=3,strides=1,padding='same',kernel_initializer='glorot_normal',activation='relu',name="{}_conv_{}".format(name,layer_dim[0]))(x)
+        
+        
+        ## downsampling layers          
+        while layer_dim[0] > 4:
+            layer_dim = np.int32(layer_dim / 2)
+            filters = filters * 2
+            x = keras.layers.BatchNormalization(name="{}_bn_{}".format(name,layer_dim[0]))(x)
+            x = keras.layers.Conv3D(filters=filters,kernel_size=3,strides=2,padding='same',kernel_initializer='glorot_normal',activation='relu',name="{}_conv_{}".format(name,layer_dim[0]))(x)
+            
+
+        ## flatten + dense layer
+        x = keras.layers.BatchNormalization(name="{}_bn_dense_{}".format(name,layer_dim[0]))(x)
+        x = keras.layers.Flatten()(x) #keras.layers.Dense(tf.math.reduce_prod(layer_dim), activation='relu', name="{}_fc_1".format(name))(keras.layers.Flatten()(x))
+        
+        
+        ## mu
+        mu = keras.layers.Dense(latent_dim, name="{}_mu".format(name))(x)
+        
+        ## sigma
+        sigma = keras.layers.Dense(latent_dim, name="{}_sigma".format(name))(x)
+        
+        ## z
+        z = Sampling(name="{}_z".format(name))([mu,sigma])
+        
+        return keras.Model(input,[mu,sigma,z],name=name), np.int32((*layer_dim,filters)), filters
+
+        
+def get_decoder(latent_dim,output_size,layer_dim,filters,name="decoder"):
+        
+        input = keras.Input(shape=(latent_dim,))
+        
+        ## unflatten + dense layer
+        x = keras.layers.Reshape(layer_dim)(keras.layers.Dense(tf.math.reduce_prod(layer_dim),name="{}_fc_1".format(name))(input))
+        x = keras.layers.BatchNormalization(name="{}_bn_dense_{}".format(name,layer_dim[0]))(x)
+
+        x = keras.layers.Conv3DTranspose(filters=filters,kernel_size=3,strides=1,padding='same',kernel_initializer='glorot_normal',activation='relu',name="{}_conv_{}".format(name,layer_dim[0]))(x)
+        x = keras.layers.BatchNormalization(name="{}_bn_{}".format(name,layer_dim[0]))(x)
+        
+        ## upsampling layers  
+        while layer_dim[0] < output_size[0]:
+            layer_dim = layer_dim * 2
+            filters = filters // 2
+            x = keras.layers.Conv3DTranspose(filters=filters,kernel_size=3,strides=2,padding='same',kernel_initializer='glorot_normal',activation='relu',name="{}_conv_{}".format(name,layer_dim[0]))(x)
+            x = keras.layers.BatchNormalization(name="{}_bn_{}".format(name,layer_dim[0]))(x)
+      
+
+        ## last conv same resulotion
+        output = keras.layers.Conv3DTranspose(filters=1, kernel_size=3,strides=1,padding='same',kernel_initializer='glorot_normal',activation='relu',name="{}_conv_{}_out".format(name,layer_dim[0]))(x)
+
+        return keras.Model(input,output,name=name)  
+    
+class VAE(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+
+    def train_step(self, data):
+
+        with tf.GradientTape() as tape:
+            mu, sigma, z = self.encoder(data[0])
+            reconstruction = self.decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    keras.losses.mean_squared_error(data[1], reconstruction),axis=(1,2)
+                )
+            )
+            kl_loss = -0.5 * (1 + sigma - tf.square(mu) - tf.exp(sigma))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }      
+    
+    def test_step(self, data):    
+        mu, sigma, z = self.encoder(data[0])
+        reconstruction = self.decoder(z)
+        reconstruction_loss = tf.reduce_mean(
+            tf.reduce_sum(
+                keras.losses.mean_squared_error(data[1], reconstruction),axis=(1,2)
+            )
+        )
+        kl_loss = -0.5 * (1 + sigma - tf.square(mu) - tf.exp(sigma))
+        kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+        total_loss = reconstruction_loss + kl_loss
+        
+
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        } 
+    
+    def call(self,input):
+        mu, sigma, z = self.encoder(input)
+        reconstruction = self.decoder(z)
+        return reconstruction
---
+++ setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+__version__ = '1.0.0'
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+requirements = [
+    "matplotlib",
+    "numpy",
+    "tifffile",
+    "scikit-image>=0.15.0",
+    "aicsimageio==3.0.7",
+    "quilt3",
+    "bumpversion",
+    "twine",
+    "setuptools>=42",
+    "wheel",
+    "pandas",
+    "multipledispatch",
+    "cell_imaging_utils",
+    "tqdm",
+    "sklearn",
+    "opencv"
+]
+
+
+setuptools.setup(
+    author="Lion Ben Nedava",
+    author_email="lionben89@gmail.com",
+    install_requires=requirements,
+    license="MIT",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="cell_generator",
+    name="cell_generator",
+    packages=setuptools.find_packages(exclude=["images"]),
+    python_requires=">=3.7",
+    test_suite="tests",
+    url="https://github.com/lionben89/cell_generator",
+    # Do not edit this string manually, always use bumpversion
+    # Details in CONTRIBUTING.rst
+    version=__version__,
+    zip_safe=False
+)

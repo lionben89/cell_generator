f34c45f934d72d3b4fc6dd165005e95e7f52adbe
lionben89 <lionben89@gmail.com>
2022-02-14 08:35:32 +0200

    AAE

--- test.py
+++ test.py
@@ -4,21 +4,34 @@ from dataset import PatchDataGen
 from models.VAE import *
 from cell_imaging_utils.image.image_utils import ImageUtils
 import matplotlib.pyplot as plt
+import global_vars as gv
+
 tf.compat.v1.enable_eager_execution()
 
 print("GPUs Available: ", tf.config.list_physical_devices('GPU'))
 
-patch_size =  (1,256,256,1)
 
-validation_dataset = PatchDataGen("/sise/home/lionb/cell_generator/image_list_test.csv","channel_signal","channel_signal",8,patch_size=patch_size)
+validation_dataset = PatchDataGen(gv.train_ds_path,"channel_signal","channel_signal",8,patch_size=gv.patch_size)
 
-vae = keras.models.load_model("./vae_single_cell")
+if (gv.model_type == "VAE"):
+    from models.VAE import *
+    vae = keras.models.load_model(gv.model_path)
 
-patch_0 = validation_dataset.__getitem__(0)[0]
-mu,sigma,sampled_z = vae.encoder(patch_0)
-sigma_zeros = tf.zeros(mu.shape)
-reconstruction_z = Sampling()([mu,sigma_zeros])
-reconstruction_patch_0 = vae.decoder(reconstruction_z).numpy()
-for i in range(patch_0.shape[0]):
-    ImageUtils.imsave(patch_0[i],"original_patch_{}.tiff".format(i))
-    ImageUtils.imsave(reconstruction_patch_0[i],"reconstruction_patch_{}.tiff".format(i))
+    patch_0 = validation_dataset.__getitem__(0)[0]
+    mu,sigma,sampled_z = vae.encoder(patch_0)
+    sigma_zeros = tf.zeros(mu.shape)
+    reconstruction_z = Sampling()([mu,sigma_zeros])
+    reconstruction_patch_0 = vae.decoder(reconstruction_z).numpy()
+    for i in range(patch_0.shape[0]):
+        ImageUtils.imsave(patch_0[i],"original_patch_{}.tiff".format(i))
+        ImageUtils.imsave(reconstruction_patch_0[i],"vae_reconstruction_patch_{}.tiff".format(i))
+
+elif (gv.model_type == "AAE"):
+    from models.AAE import *
+    aae = keras.models.load_model(gv.model_path)
+
+    patch_0 = validation_dataset.__getitem__(0)[0]
+    reconstruction_patch_0 = aae.generator(patch_0).numpy()
+    for i in range(patch_0.shape[0]):
+        ImageUtils.imsave(patch_0[i],"original_patch_{}.tiff".format(i))
+        ImageUtils.imsave(reconstruction_patch_0[i],"aae_reconstruction_patch_{}.tiff".format(i))
--- train.py
+++ train.py
@@ -1,30 +1,49 @@
 import tensorflow as tf
 import tensorflow.keras as keras
 from dataset import PatchDataGen
-from models.VAE import *
-import matplotlib.pyplot as plt
+import global_vars as gv
+from models.AAE import get_discriminator
 tf.compat.v1.enable_eager_execution()
 
-CONTINUE_TRAINING = False
+# CONTINUE_TRAINING = False
+CONTINUE_TRAINING = True
 
 print("GPUs Available: ", tf.config.list_physical_devices('GPU'))
-model_path = "./vae_single_cell_2d"
-patch_size = (1,256,256,1) ## 2D: (1,*,*,1) 3D: (*,*,*,1)
-latent_dim = 64
-train_dataset = PatchDataGen("/sise/home/lionb/cell_generator/image_list_train.csv","channel_signal","channel_signal",128,patch_size=patch_size)
-validation_dataset = PatchDataGen("/sise/home/lionb/cell_generator/image_list_test.csv","channel_signal","channel_signal",8,patch_size=patch_size)
 
-encoder, layer_dim, filters = get_encoder(patch_size,latent_dim)
-encoder.summary()
-decoder = get_decoder(latent_dim,patch_size,layer_dim,filters)
-decoder.summary()
-vae = VAE(encoder,decoder)
-vae.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001))
+train_dataset = PatchDataGen(gv.train_ds_path,"channel_signal","channel_signal",128,patch_size=gv.patch_size)
+validation_dataset = PatchDataGen(gv.test_ds_path,"channel_signal","channel_signal",8,patch_size=gv.patch_size)
+if (gv.model_type == "VAE"):
+    from models.VAE import *
+    encoder, layer_dim, filters = get_encoder(gv.patch_size,gv.latent_dim)
+    encoder.summary()
+    decoder = get_decoder(gv.latent_dim,gv.patch_size,layer_dim,filters)
+    decoder.summary()
+    vae = VAE(encoder,decoder,beta=0.5) ## beta for reconstruction 1 for KL
+    vae.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001))
 
-if CONTINUE_TRAINING:
-    vae_pt = keras.models.load_model(model_path)
-    vae.set_weights(vae_pt.get_weights())
+    if CONTINUE_TRAINING:
+        vae_pt = keras.models.load_model(gv.model_path)
+        vae.set_weights(vae_pt.get_weights())
+        
+    checkpoint_callback = keras.callbacks.ModelCheckpoint(gv.model_path, save_best_only=True, save_weights_only=True, save_format="tf")
+    early_stop_callback = keras.callbacks.EarlyStopping(patience=20)
+    callbacks = [checkpoint_callback,early_stop_callback]
+    vae.fit(train_dataset,validation_data=validation_dataset, epochs=100, callbacks=callbacks) ## validation_data=validation_dataset,
+    vae.save(gv.model_path,save_format="tf")
     
-# callback = keras.callbacks.ModelCheckpoint("./vae_single_cell.h5",monitor="val_loss", save_best_only=True)
-vae.fit(train_dataset,validation_data=validation_dataset, epochs=700)#, callbacks=callback) ## validation_data=validation_dataset,
-vae.save(model_path,save_format="tf")
+elif (gv.model_type == "AAE"):
+    from models.AAE import *
+    encoder, layer_dim, filters = get_encoder(gv.patch_size,gv.latent_dim)
+    encoder.summary()
+    decoder = get_decoder(gv.latent_dim,gv.patch_size,layer_dim,filters)
+    decoder.summary()
+    discriminator = get_discriminator(gv.latent_dim)
+    discriminator.summary()
+    aae = AAE(encoder,decoder,gv.patch_size,discriminator)
+    aae.compile(keras.optimizers.Adam(learning_rate=0.00001), keras.optimizers.Adam(learning_rate=0.0001))
+
+    if CONTINUE_TRAINING:
+        aae_pt = keras.models.load_model(gv.model_path)
+        aae.set_weights(aae_pt.get_weights())
+    aae.fit(train_dataset, epochs=1000)
+    aae.save(gv.model_path,save_format="tf")
